d286b7e7e0d833de97987552c7ba5d4f28ca355a
unknown <ashyr.muhammedov@gmail.com>
2021-09-17 19:57:35 +0300

    Изменения внесены

--- Task#1.1/main.cs
+++ Task#1.1/main.cs
@@ -6,17 +6,18 @@ class Program {
     string [] stringArray;
     int len;
 
-    public stringList (int g) {
-      this.len = g;
-      this.stringArray = new string[g];
-      for (int i = 0; i < this.len; i++) stringArray[i] = "(empty)"; 
+    public stringList () {
+      this.len = 100;
+      this.stringArray = new string[100]; 
     }
     
     public int Insert (string elem) {
       
       int i;
 
-      for (i = 0; i < len && stringArray[i] != "(empty)"; i++);
+      for (i = 0; i < len; i++) {
+        if ( String.IsNullOrEmpty(stringArray[i]) ) break;
+      }
 
       if (i == len) return 0;
       
@@ -29,43 +30,65 @@ class Program {
       
       if (num >= len) return;
 
-      stringArray[num] = "(empty)";
+      stringArray[num] = String.Empty;
     }
 
     public int Search (string elem) {
 
-      for (int i = 0; i < len; i++) {
-        if (stringArray[i] == elem) return 1;
-      }
+      for (int i = 0; i < len; i++) 
+        if (stringArray[i] == elem) {
+          Console.WriteLine("Found string \""+elem+"\" at position "+ i);
+          return 1;
+        }
       
+      Console.WriteLine("String \""+elem+"\" not found!");
       return 0;
     }
+
     public void Update (int num, string elem) {
       
       if (num >= len) return;
       
       stringArray[num] = elem;
     
-    } 
+    }
+
     public string GetAt (int num) {
     
       if (num >= len) return "Fail";
     
+      if (String.IsNullOrEmpty(stringArray[num])) return "no element";
+
       return stringArray[num];
     }
+
+    public void PrintAt (int g) {
+      Console.WriteLine ("#"+ g +": " + this.GetAt (g));
+    }
+
     public void Print () {
-      for (int i = 0; i < len; i++) {
-        Console.WriteLine(stringArray[i]);
-      }
+        for (int i=0; i<this.len; i++) this.PrintAt(i);
     }
   }
+  
+  
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    var a = new stringList (25);
+    
+    var a = new stringList ();
 
     a.Insert ("test");
     a.Insert ("test 2");
     a.Insert ("test 3");
-    a.Print ();
+    a.Insert ("test 4");
+
+    a.Delete (2);
+
+
+
+    a.Print();
+
+    a.Search ("test");
+    a.Search ("test 3");
+
   }
 }
