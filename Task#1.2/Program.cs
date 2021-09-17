3e11d076bb5ce43a67c477387fabfeb40a77b055
Sergey Shakotko <s.shakotko@city-mobil.ru>
2021-09-18 01:02:05 +0300

    Сделана таска

---
+++ Task#1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DifferenceMatter diff = new DifferenceMatter();
+
+            // Console.WriteLine(diff.i); // Не можем так обратиться, т.к константа является неявно статическим членом
+            Console.WriteLine(DifferenceMatter.i);
+
+            Console.WriteLine(diff.ReturnConstant());
+
+            Console.WriteLine(diff.s.ReturnReadOnly());
+        }
+    }
+}
