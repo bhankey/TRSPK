578ef1fc4e853513c24d784fff8f7d6e3f192bda
bhankey <sergeysh2001@gmail.com>
2021-09-26 18:14:25 +0300

    Move All to project App

--- CalorieContent/CalorieContentMain/lib/KeyValueStorage/Storage.cs
+++ CalorieContent/CalorieContentMain/lib/KeyValueStorage/Storage.cs
@@ -8,7 +8,7 @@ namespace CalorieContent.lib.KeyValueStorage
     {
         private const int CountOfDataBases = 2;
 
-        private const string _basePathToFile = "C:\\Users\\Sergey\\RiderProjects\\TRSPK\\CalorieContent\\DB";
+        private const string _basePathToFile = "C:\\Users\\Sergey\\Documents\\Charps\\TRSPK\\CalorieContent\\CalorieContentMain\\DB\\DB";
 
 
         private static readonly FileStream[] db = new FileStream[CountOfDataBases];
