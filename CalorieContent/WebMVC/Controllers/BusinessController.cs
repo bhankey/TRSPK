44cb2297e5f7c7b157fecdf80cd5a64f3ea56934
bhankey <sergeysh2001@gmail.com>
2021-10-16 15:19:33 +0300

    Add try catch in businessLogic controller

--- CalorieContent/WebMVC/Controllers/BusinessController.cs
+++ CalorieContent/WebMVC/Controllers/BusinessController.cs
@@ -44,66 +44,12 @@ namespace WebMVC.Controllers
             {
                 case serviceType.RecipeDetails:
                 {
-                    var recipe = RecipeService.DescribeRecipe(input);
-
-                    StringBuilder result = new StringBuilder();
-
-                    result.Append("Recipe name: ");
-                    result.Append(recipe.RecipeName);
-                    result.Append("\n");
-
-                    result.Append("Ingredients: \n");
-                    foreach (var i in recipe.IngredientCalorie)
+                    try
                     {
-                        result.Append("\t");
-                        result.Append(i.Key);
-                        result.Append(" - ");
-                        result.Append(i.Value);
-                        result.Append(" calorie\n");
-                    }
+                        var recipe = RecipeService.DescribeRecipe(input);
 
-                    ViewData["ResultOfService"] = result.ToString();
-                    
-                    break;   
-                }
-                case serviceType.LowerCalorieContentThan:
-                {
-                    var recipes = RecipeService.GetLessSumCalorie(int.Parse(input));
+                        StringBuilder result = new StringBuilder();
 
-                    var result = new StringBuilder();
-                    foreach (var recipe in recipes)
-                    {
-                        result.Append("Recipe name: ");
-                        result.Append(recipe.Name);
-                        result.Append("\n");
-
-                        result.Append("Ingredients: \n");
-                        foreach (var ingredient in recipe.Ingredients)
-                        {
-                            result.Append("\t");
-                            result.Append(ingredient.Name);
-                            result.Append(" - ");
-                            result.Append(ingredient.Grams);
-                            result.Append(" grams\n");
-                        }
-                    }
-                    
-                    if (recipes.Count == 0)
-                    {
-                        result.Append("No recipes lower than that calorie: " + int.Parse(input));
-                    }
-
-                    ViewData["ResultOfService"] = result.ToString();
-                    
-                    break;
-                }
-                case serviceType.ConsistOf:
-                {
-                    var recipes = RecipeService.GetRecipesByIngredients(new List<String>(input.Split(',')));
-
-                    StringBuilder result = new StringBuilder();
-                    foreach (var recipe in recipes)
-                    {
                         result.Append("Recipe name: ");
                         result.Append(recipe.RecipeName);
                         result.Append("\n");
@@ -118,15 +64,104 @@ namespace WebMVC.Controllers
                             result.Append(" calorie\n");
                         }
 
+                        ViewData["ResultOfService"] = result.ToString();
                     }
-                    
-                    if (recipes.Count == 0)
+                    catch (Exception e)
                     {
-                        result.Append("No recipes consist of this ingredients: " + input.Split(','));
+                        ViewData["ResultOfService"] = e.Message;
                     }
-                    
-                    ViewData["ResultOfService"] = result.ToString();
-                    
+
+                    break;   
+                }
+                case serviceType.LowerCalorieContentThan:
+                {
+                    try
+                    {
+                        var recipes = RecipeService.GetLessSumCalorie(int.Parse(input));
+
+                        var result = new StringBuilder();
+                        foreach (var recipe in recipes)
+                        {
+                            result.Append("Recipe name: ");
+                            result.Append(recipe.Name);
+                            result.Append("\n");
+
+                            result.Append("Ingredients: \n");
+                            foreach (var ingredient in recipe.Ingredients)
+                            {
+                                result.Append("\t");
+                                result.Append(ingredient.Name);
+                                result.Append(" - ");
+                                result.Append(ingredient.Grams);
+                                result.Append(" grams\n");
+                            }
+                        }
+
+                        if (recipes.Count == 0)
+                        {
+                            result.Append("No recipes lower than that calorie: " + int.Parse(input));
+                        }
+
+                        ViewData["ResultOfService"] = result.ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        ViewData["ResultOfService"] = e.Message;
+                    }
+
+                    break;
+                }
+                case serviceType.ConsistOf:
+                {
+                    try
+                    {
+                        var recipes = RecipeService.GetRecipesByIngredients(new List<String>(input.Split(',')));
+
+                        StringBuilder result = new StringBuilder();
+                        foreach (var recipe in recipes)
+                        {
+                            result.Append("Recipe name: ");
+                            result.Append(recipe.RecipeName);
+                            result.Append("\n");
+
+                            result.Append("Ingredients: \n");
+                            foreach (var i in recipe.IngredientCalorie)
+                            {
+                                result.Append("\t");
+                                result.Append(i.Key);
+                                result.Append(" - ");
+                                result.Append(i.Value);
+                                result.Append(" calorie\n");
+                            }
+
+                        }
+
+                        if (recipes.Count == 0)
+                        {
+                            result.Append("No recipes consist of this ingredients: ");
+                        }
+
+                        var split = input.Split(',');
+                        for (var i = 0; i < split.Length; i++)
+                        {
+                            var ingredient = split[i];
+                            if (i != split.Length - 1)
+                            {
+                                result.Append(ingredient + ',');
+                            }
+                            else
+                            {
+                                result.Append(ingredient + '\n');
+                            }
+                        }
+
+                        ViewData["ResultOfService"] = result.ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        ViewData["ResultOfService"] = e.Message;
+                    }
+
                     break;
                 }
             }
